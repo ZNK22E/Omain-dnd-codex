bf2d65836d27644f79cb276baf0976d239b069b2
Kitchvx <nathan@nkitch.com>
2025-11-23 16:48:27 +0000

    Astro config updated to reflect github pages as the site with the repo as the base.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
 export default defineConfig({
+  site: "https://ZNK22E.github.io/Omain-dnd-codex",
+  base: "/Omain-dnd-codex",
   integrations: [
     starlight({
       title: "The Grand Archives of Sacro Elcarion",
